121d74af8422d7e61566dc1f2eb3a295e5e36703
Alberto Carrizo <ae_carrizo@hotmail.com>
2024-10-07 17:25:03 -0300

    Error 504

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { textToImage } from "@/utils/textToImage";
-import {saveAs} from "file-saver"
+import { saveAs } from "file-saver"
 
 export default function Home() {
     const [animal, setAnimal] = useState('')
@@ -11,8 +11,8 @@ export default function Home() {
     const [imageURL, setImageURL] = useState('/Lettest.jpg')
     const [change, setChange] = useState(false)
 
-    const SaveFile=()=>{
-        saveAs(imageURL,'GeneradaIA.jpg')
+    const SaveFile = () => {
+        saveAs(imageURL, 'GeneradaIA.jpg')
     }
 
     const aiBlob = async (e) => {
@@ -20,7 +20,8 @@ export default function Home() {
         setChange(true)
         const imgURL = await textToImage(animal, text)
         console.log('IMAGEN URL', imgURL)
-        setImageURL(imgURL)
+        if (imgURL === undefined) setImageURLmageURL('/Error504.jpg')
+        else { setImageURL(imgURL) }
         setLoading(true)
         setChange(false)
     }
@@ -48,12 +49,12 @@ export default function Home() {
                 <img src="/Engranaje.png" width={200} height={200} className="mx-auto animate-spin mt-24" alt="Engranaje" />
             )}
             {!change && (
-                <img src={imageURL} width={512} height={512} className="mx-auto mt-4 rounded-xl" alt={change} />
+                <img src={imageURL} width={512} height={512} className="mx-auto mt-4 rounded-xl" alt={imageURL} />
             )}
             {loading && (
                 <button className="w-52 bg-green-600 text-slate-950 text-xl p-4 rounded-xl border-solid border-4 border-green-900 hover:bg-green-400 mx-auto mt-4"
-                onClick={SaveFile}>
-                   Descargar Imagen 
+                    onClick={SaveFile}>
+                    Descargar Imagen
                 </button>
             )}
 
--- utils/textToImage.js
+++ utils/textToImage.js
@@ -6,7 +6,6 @@ const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.
 
 export async function textToImage(animal, text) {
     const Token = process.env.API_TOKEN
-    console.log('token : animal : text:', Token, animal, text)
     const response = await fetch(
         "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
         {
@@ -17,19 +16,14 @@ export async function textToImage(animal, text) {
             method: "POST",
             body: JSON.stringify({
                 inputs: `a gift card that cover all the image with the image of a ${animal} and this text '${text}' above, on the top of the card`,
-                image_size: '16_9',
-                height: 960,
-                width: 540,
-                guidance_scale: 3.5,
-                num_inference_steps: 50,
-                max_sequence_length: 512,
-                generator: { seed: 1 }
+                go_fast: true,
+
+               
             }
             ),
         }
     );
     const blob = await response.blob();
-    console.log('BLOB:::', blob)
     const arrayBuffer = await blob.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
 
@@ -42,7 +36,6 @@ export async function textToImage(animal, text) {
         console.error("Error al subir el archivo a Supabase:", error);
         return null;
     }
-    console.log('PATH', path)
     const { data: fileURL } = supabase.storage.from("cms-Main").getPublicUrl(path.path)
     const timestamp = new Date().getTime()
     return `${fileURL.publicUrl}?t=${timestamp}`
